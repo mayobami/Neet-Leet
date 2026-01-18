5f230c3d2aafe2b97d9ce8dc1a106da28221a580
mayo <88555602+mayobami@users.noreply.github.com>
2026-01-18 16:11:56 +0000

    Update remove-element.py

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -9,4 +9,20 @@ class Solution:
 
         return k
 
+#alternative
 
+nums = [0,1,2,2,3,0,4,2]
+vals = 3
+
+def removeElement(nums, vals):
+    x=0
+    for i in nums:
+        if nums[x] == vals:
+            nums[x] = 0
+        x+=1
+    return nums
+
+
+new_list = [1,2,2]
+num = 2
+removeElement(new_list, num)
